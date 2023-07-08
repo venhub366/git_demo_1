69fe2080db6eb717b93aac4249ad7e3cb3eb0327
venkuuzHUB <kumaraswamyvenkatesh@gmail.com>
2023-07-08 08:47:41 +0530

    Commit - Changes for US5332

---
+++ src/main/java/com/yaml/load/tests/TwoSum.java
@@ -0,0 +1,17 @@
+package com.yaml.load.tests;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+
+    public static void main(String[] args) {
+
+        int target = 7;
+        HashMap<Integer,Integer> tempMap = new HashMap<Integer, Integer>();
+        int[] arr = {1,3,4,5,9};
+        for(int i = 0 ; i < arr.length ; i++){
+            //add logic
+        }
+    }
+}
